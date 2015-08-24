f34f226ac2e99afab5e0ff5243d8c7c6e3bb0f57
idmarjr <idmarjr@gmail.com>
2015-08-24 16:47:58 -0300

    Making clear when each installation start and end.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 activatorVersion="1.3.5"
 sbtVersion="0.13.9"
 
+echo "=========================================="
 echo "Provision VM START"
 echo "=========================================="
 
@@ -66,69 +67,74 @@ ln -s /usr/bin/nodejs /user/bin/node
 sudo apt-get -y install npm
 
 ###############################################
-#install CoffeeScript
+# Install CoffeeScript
 ###############################################
 sudo npm install coffee-script
 
 ###############################################
-#install Bower
+# Install Bower
 ###############################################
 npm install bower -g
 
 ###############################################
-#install SBT
-###############################################
-wget http://dl.bintray.com/sbt/debian/sbt-$sbtVersion.deb
-sudo dpkg -i sbt-$sbtVersion.deb
-sudo apt-get update
-sudo apt-get install sbt
-rm sbt-$sbtVersion.deb
-
-###############################################
-#install Redis
+# Install Redis
 # More info about it: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-redis
 ###############################################
-echo "Download redis..."
+echo "Download Redis..."
 wget http://download.redis.io/releases/redis-stable.tar.gz
 tar xzf redis-stable.tar.gz
-echo "Init install..."
 cd redis-stable
 make
 make test
 sudo make install
 cd utils
 sudo ./install_server.sh
-echo "Redis done"
+rm redis-stable.tar.gz
+echo "Redis done."
 
 ###############################################
-#install MongDB
+# Install MongDB
 ###############################################
+echo "Download MongoDB..."
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
 sudo apt-get -y install mongodb-org
+echo "MongDB done."
 
 ###############################################
-#install typesafe activator
+# Install SBT
+###############################################
+echo "Download SBT..."
+wget http://dl.bintray.com/sbt/debian/sbt-$sbtVersion.deb
+sudo dpkg -i sbt-$sbtVersion.deb
+sudo apt-get update
+sudo apt-get install sbt
+rm sbt-$sbtVersion.deb
+echo "SBT done."
+
+###############################################
+# Install typesafe activator
 ###############################################
 cd /home/vagrant
+echo "Download Typesafe Activator..."
 wget http://downloads.typesafe.com/typesafe-activator/$activatorVersion/typesafe-activator-$activatorVersion-minimal.zip
 unzip -d /home/vagrant typesafe-activator-$activatorVersion-minimal.zip
 rm typesafe-activator-$activatorVersion-minimal.zip
-#export PATH=/home/vagrant/activator-${activatorVersion}-minimal:$PATH >> ~/.bashrc
+echo "Typesafe Activator done."
 
 ###############################################
-#add activator to environment variables
+# Add activator to environment variables
 ###############################################
 echo "export PATH=/home/vagrant/activator-$activatorVersion-minimal:\$PATH" >> ~/.bashrc
 
 ###############################################
-#use node as default JavaScript Engine
+# Use node as default JavaScript Engine
 ###############################################
 echo "export SBT_OPTS=\"\$SBT_OPTS -Dsbt.jse.engineType=Node\"" >> ~/.bashrc
 
 ###############################################
-#reset bash
+# Reset bash
 ###############################################
 source ~/.bashrc
 
@@ -138,9 +144,11 @@ source ~/.bashrc
 ###############################################
 /home/vagrant/activator-$activatorVersion-minimal/activator help
 
-echo ""
-echo ""
+###############################################
+# Show installation sumsary
+###############################################
 echo "=========================================="
+echo "Provision VM sumsary"
 echo "=========================================="
 echo "Dependencies installed:"
 echo "jdk version:"
@@ -158,7 +166,5 @@ echo ""
 echo "NPM version"
 npm -v
 echo "=========================================="
-echo "=========================================="
-echo ""
-echo ""
 echo "Provision VM finished"
+echo "=========================================="
