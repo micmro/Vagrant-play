33d8a5351869b0874612220e285dddce56e4f9e7
idmarjr <idmarjr@gmail.com>
2015-09-02 16:37:01 -0300

    Keep mongoDB commented until I find whats going wrong. :/

--- vagrant-machine-setup.sh
+++ vagrant-machine-setup.sh
@@ -106,12 +106,12 @@ echo "Redis done."
 ###############################################
 # Install MongDB
 ###############################################
-echo "Download MongoDB..."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-sudo apt-get update
-sudo apt-get -y install mongodb-org
-echo "MongDB done."
+# echo "Download MongoDB..."
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+# echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+# sudo apt-get update
+# sudo apt-get -y install mongodb-org
+# echo "MongDB done."
 
 ###############################################
 # Install MongDB Clients
