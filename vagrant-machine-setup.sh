7c37e6ea2199ed14803eec5304126a93b11c7b24
idmarjr <idmarjr@gmail.com>
2015-09-21 17:53:04 -0300

    Install NodeJS 4.x + export node_modules to path

--- vagrant-machine-setup.sh
+++ vagrant-machine-setup.sh
@@ -58,8 +58,11 @@ sudo apt-get -y install unzip
 ###############################################
 # Install NodeJS
 ###############################################
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 sudo apt-get -y install nodejs
 ln -s /usr/bin/nodejs /user/bin/node
+# Add node_modules to environment variables
+echo "export NODE_PATH=/usr/local/lib/node_modules" >> ~/.bashrc
 
 ###############################################
 # Install Node related stuff required by Play
