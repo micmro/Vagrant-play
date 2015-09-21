8facd17fa7d9394c15cf1e88683fe1e0cef94972
idmarjr <idmarjr@gmail.com>
2015-09-21 17:57:52 -0300

    Remove mongoDB and mongodb-clients from shell script

--- vagrant-machine-setup.sh
+++ vagrant-machine-setup.sh
@@ -102,21 +102,6 @@ rm redis-stable.tar.gz
 echo "Redis done."
 
 ###############################################
-# Install MongDB
-###############################################
-# echo "Download MongoDB..."
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-# echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-# sudo apt-get update
-# sudo apt-get -y install mongodb-org
-# echo "MongDB done."
-
-###############################################
-# Install MongDB Clients
-###############################################
-sudo apt-get -y install mongodb-clients
-
-###############################################
 # Install PostgreSQL
 ###############################################
 sudo apt-get -y install postgresql postgresql-contrib postgresql-client-common postgresql-common
