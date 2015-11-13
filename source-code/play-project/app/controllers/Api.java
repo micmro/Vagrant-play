2cfb257c7cc2b2a5019030c89f1c911e96c59d89
Fellipe Souto <fllsouto@gmail.com>
2015-11-13 00:57:34 -0200

    Saving all the refactoring made until now

---
+++ source-code/play-project/app/controllers/Api.java
@@ -0,0 +1,154 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+import views.html.*;
+import models.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import enums.ShortLink;
+
+import java.io.*;
+
+public class Api extends Controller {
+
+  public Result shortlinkBase(String username, String slType) {
+
+    System.out.println("Received type : " + slType);
+    ShortLink sl = ShortLink.valueOfSl(slType);
+    switch(sl) {
+      case SHORTEN:
+        return ok(views.html.shortlink.shorten.render(username));
+      case TEMPORARY:
+        return ok(views.html.shortlink.temporary.render(username));
+      case CUSTOMIZE:
+        return ok(views.html.shortlink.customize.render(username));
+      case SECURE:
+        return ok(views.html.shortlink.secure.render(username));
+      default:
+        return redirect(controllers.routes.Users.account(username));
+    }
+  }
+
+  public Result shorten(String username) {
+
+    String shortened = null, original = Form.form().bindFromRequest().get("originalLink");
+
+    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
+      return forbidden("You are not allowed to access this route directly from the browser");
+
+    if(original != null && !original.isEmpty()) {
+      Tunnel tnl = new Tunnel();
+      shortened = tnl.dig(original, username);
+
+      URL url = new URL();
+      url.set_owner(username);
+      url.set_generated(shortened);
+      url.set_original(original);
+      url.set_creation(LocalDateTime.now());
+      url.set_type('s');
+      url.save();
+
+      return redirect(controllers.routes.Users.result(username, original, shortened));
+    }
+
+    return ok(views.html.shortlink.shorten.render(username));
+  }
+
+  public Result customize(String username) {
+
+    String customized = Form.form().bindFromRequest().get("customizedLink");
+    String original = Form.form().bindFromRequest().get("originalLink");
+
+    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
+      return forbidden("You are not allowed to access this route directly from the browser");
+
+    /*TODO: check if customized is already in use here*/
+
+    if(original != null && !original.isEmpty() && customized != null && !customized.isEmpty()) {
+
+      URL url = new URL();
+      url.set_owner(username);
+      url.set_generated(customized);
+      url.set_original(original);
+      url.set_creation(LocalDateTime.now());
+      url.set_type('c');
+      url.save();
+
+      return redirect(controllers.routes.Users.result(username, original, customized));
+    }
+
+    return ok(views.html.shortlink.customize.render(username));
+  }
+
+  public Result secure(String username) {
+
+    String secured = Form.form().bindFromRequest().get("securedLink");
+    String password = Form.form().bindFromRequest().get("goldenShovel");
+    String original = Form.form().bindFromRequest().get("originalLink");
+
+    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
+      return forbidden("You are not allowed to access this route directly from the browser");
+
+    /*TODO: check if secured is already in use here*/
+
+    if(original != null && !original.isEmpty() && secured != null && !secured.isEmpty() && password != null && !password.isEmpty()) {
+
+      URL url = new URL();
+      url.set_owner(username);
+      url.set_generated(secured);
+      url.set_original(original);
+      url.set_creation(LocalDateTime.now());
+      url.set_type('p');
+      url.set_password(password);
+      url.save();
+
+      return redirect(controllers.routes.Users.result(username, original, secured));
+    }
+
+    return ok(views.html.shortlink.secure.render(username));
+  }
+
+  public Result temporary(String username) {
+
+    String timed = Form.form().bindFromRequest().get("timedLink");
+    String expiration = Form.form().bindFromRequest().get("lifetime");
+    String original = Form.form().bindFromRequest().get("originalLink");
+
+    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
+      return forbidden("You are not allowed to access this route directly from the browser");
+
+    /*TODO: check if timed is already in use here*/
+
+    if(original != null && !original.isEmpty() && timed != null && !timed.isEmpty() && expiration != null && !expiration.isEmpty()) {
+      LocalDateTime d;
+
+      URL url = new URL();
+      url.set_owner(username);
+      url.set_generated(timed);
+      url.set_original(original);
+      url.set_creation(LocalDateTime.now());
+      url.set_type('t');
+      if(expiration.charAt(0) == 'm') {
+        d = url.get_creation();
+        url.set_expiration(d.plusMinutes(Long.parseLong(expiration.substring(1))));
+      }
+      else if(expiration.charAt(0) == 'h') {
+        d = url.get_creation();
+        url.set_expiration(d.plusHours(Long.parseLong(expiration.substring(1))));
+      }
+      else if(expiration.charAt(0) == 'd') {
+        d = url.get_creation();
+        url.set_expiration(d.plusDays(Long.parseLong(expiration.substring(1))));
+      }
+      url.save();
+
+      return redirect(controllers.routes.Users.result(username, original, timed));
+    }
+
+    return ok(views.html.shortlink.temporary.render(username));
+  }
+
+}
--- source-code/play-project/app/controllers/Users.java
+++ source-code/play-project/app/controllers/Users.java
@@ -41,8 +41,7 @@ public class Users extends Controller {
       if(User.authenticate(username, password)) {
         if(!online.contains(username)) online.add(username);
         session().remove("Username"); //Para que o session username seja atualizado caso novo usuario de sign in
-        String username_when_logged = "Hello, " + username + "!";
-        session("Username",username_when_logged);
+        session("Username",username);
         return redirect(controllers.routes.Users.account(username));
       }
       else
@@ -52,84 +51,6 @@ public class Users extends Controller {
     return ok(views.html.user.signin.render());
   }
 
-  public Result shorten(String username)
-  {
-    String shortened = null, original = Form.form().bindFromRequest().get("originalLink");
-
-    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
-      return forbidden("You are not allowed to access this route directly from the browser");
-
-    if(original != null && !original.isEmpty()) {
-      Tunnel tnl = new Tunnel();
-      shortened = tnl.dig(original, username);
-
-      URL url = new URL();
-      url.set_owner(username);
-      url.set_generated(shortened);
-      url.set_original(original);
-      url.set_creation(LocalDateTime.now());
-      url.set_type('s');
-      url.save();
-
-      return redirect(controllers.routes.Users.result(username, original, shortened));
-    }
-
-    return ok(views.html.user.shorten.render(username));
-  }
-
-  public Result customize(String username)
-  {
-    String customized = Form.form().bindFromRequest().get("customizedLink");
-    String original = Form.form().bindFromRequest().get("originalLink");
-
-    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
-      return forbidden("You are not allowed to access this route directly from the browser");
-
-    /*TODO: check if customized is already in use here*/
-
-    if(original != null && !original.isEmpty() && customized != null && !customized.isEmpty()) {
-
-      URL url = new URL();
-      url.set_owner(username);
-      url.set_generated(customized);
-      url.set_original(original);
-      url.set_creation(LocalDateTime.now());
-      url.set_type('c');
-      url.save();
-
-      return redirect(controllers.routes.Users.result(username, original, customized));
-    }
-
-    return ok(views.html.user.customize.render(username));
-  }
-
-  public Result secure(String username)
-  {
-    String secured = Form.form().bindFromRequest().get("securedLink");
-    String password = Form.form().bindFromRequest().get("goldenShovel");
-    String original = Form.form().bindFromRequest().get("originalLink");
-
-    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
-      return forbidden("You are not allowed to access this route directly from the browser");
-
-    /*TODO: check if secured is already in use here*/
-
-    if(original != null && !original.isEmpty() && secured != null && !secured.isEmpty() && password != null && !password.isEmpty()) {
-
-      URL url = new URL();
-      url.set_owner(username);
-      url.set_generated(secured);
-      url.set_original(original);
-      url.set_creation(LocalDateTime.now());
-      url.set_type('p');
-      url.set_password(password);
-      url.save();
-
-      return redirect(controllers.routes.Users.result(username, original, secured));
-    }
-
-    return ok(views.html.user.secure.render(username));
-  }
 
   public Result list_all(String username)
   {
@@ -140,47 +61,6 @@ public class Users extends Controller {
 
     return ok(views.html.user.list_all.render(listing,username));
   }
-
-  public Result temporary(String username)
-  {
-    String timed = Form.form().bindFromRequest().get("timedLink");
-    String expiration = Form.form().bindFromRequest().get("lifetime");
-    String original = Form.form().bindFromRequest().get("originalLink");
-
-    if(request().getHeader("referer") == null || !request().getHeader("referer").contains("/" + username))
-      return forbidden("You are not allowed to access this route directly from the browser");
-
-    /*TODO: check if timed is already in use here*/
-
-    if(original != null && !original.isEmpty() && timed != null && !timed.isEmpty() && expiration != null && !expiration.isEmpty()) {
-      LocalDateTime d;
-
-      URL url = new URL();
-      url.set_owner(username);
-      url.set_generated(timed);
-      url.set_original(original);
-      url.set_creation(LocalDateTime.now());
-      url.set_type('t');
-      if(expiration.charAt(0) == 'm') {
-        d = url.get_creation();
-        url.set_expiration(d.plusMinutes(Long.parseLong(expiration.substring(1))));
-      }
-      else if(expiration.charAt(0) == 'h') {
-        d = url.get_creation();
-        url.set_expiration(d.plusHours(Long.parseLong(expiration.substring(1))));
-      }
-      else if(expiration.charAt(0) == 'd') {
-        d = url.get_creation();
-        url.set_expiration(d.plusDays(Long.parseLong(expiration.substring(1))));
-      }
-      url.save();
-
-      return redirect(controllers.routes.Users.result(username, original, timed));
-    }
-
-    return ok(views.html.user.temporary.render(username));
-  }
-
 
   /*NOTE: This method is supposed to handle the 4 types of links!*/
   public Result result(String username, String original, String generated) {
@@ -224,11 +104,13 @@ public class Users extends Controller {
 
   public Result account(String username)
   {
-    Form<User> form = Form.form(User.class);
-    if(request().getHeader("referer") == null || (!request().getHeader("referer").contains("/signin") && !request().getHeader("referer").contains("/u/" + username)))
-      /*TODO: Raise "You are not allowed to access this route directly from browser"*/
-      return forbidden("You are not allowed to access this route directly from the browser");
-    else return ok(views.html.user.actions.render(username));
+    if(username != null && online.contains(username)) {
+      return ok(views.html.user.actions.render());
+    } else {
+      flash("login_error","You must be logged in to do that!");
+      return redirect(controllers.routes.Users.signin());
+    }
+
   }
 
   public Result redir(String username, String shovels)
